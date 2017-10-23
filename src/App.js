ec926d28b37e554f59c897854859727581836ab9
Malcolm <mcconaghym@live.co.uk>
2017-10-23 20:15:11 +0100

    Initialise React Files

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './css/App.css';
+import './css/bootstrap.css';
+import './css/bootstrap-grid.css';
+import './css/bootstrap-reboot.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="container">
+              <header>
+                <h1>SickNote</h1>
+                <div className="subtitle">
+                  For when you're too sick to even email
+                </div>
+              </header>
+              <main>
+                
+              </main>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
